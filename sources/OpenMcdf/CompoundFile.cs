9e8a2830020f751b335c23d2ba4ccb7ff52abf23
Per Rovegård <per@rovegard.se>
2021-08-28 14:54:54 +0200

    Remove unused class CFItemComparer

--- sources/OpenMcdf/CompoundFile.cs
+++ sources/OpenMcdf/CompoundFile.cs
@@ -15,17 +15,6 @@ using RedBlackTree;
 
 namespace OpenMcdf
 {
-    internal class CFItemComparer : IComparer<CFItem>
-    {
-        public int Compare(CFItem x, CFItem y)
-        {
-            // X CompareTo Y : X > Y --> 1 ; X < Y  --> -1
-            return (x.DirEntry.CompareTo(y.DirEntry));
-
-            //Compare X < Y --> -1
-        }
-    }
-
     /// <summary>
     /// Configuration parameters for the compund files.
     /// They can be OR-combined to configure 
